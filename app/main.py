b28f54dd6311cbfe43dcac4d83e0afd1d38b14ca
sabmus <sabmus8@gmail.com>
2022-12-12 23:43:32 -0300

    added docker

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ class Post(BaseModel):
 
 
 my_posts = [
-    {"title": "title of post 1", "content": "content of post 1", "id": 1},
+    {"title": "title of post 1", "content": "content of post 1, using docker", "id": 1},
     {"title": "title of post 2", "content": "content of post 2", "id": 2}
 ]
 
